607d9f476bd2973cd33a2232c34428610fed78a4
Zdenek Zambersky <zzambers@redhat.com>
2021-10-07 17:18:50 +0200

    JavaScreenshoter/JavaScreenshoter.java: indetion fixes

--- JavaScreenshoter/JavaScreenshoter.java
+++ JavaScreenshoter/JavaScreenshoter.java
@@ -8,10 +8,10 @@ import javax.imageio.ImageIO;
 public class JavaScreenshoter {
 
     public static void main(String[] args) throws Exception {
-    	Robot robot = new Robot();
-    	BufferedImage image = robot.createScreenCapture(new Rectangle(0, 0, 512, 512));
-    	File file = new File("screenshot.png");
-    	ImageIO.write(image, "png", file);
+        Robot robot = new Robot();
+        BufferedImage image = robot.createScreenCapture(new Rectangle(0, 0, 512, 512));
+        File file = new File("screenshot.png");
+        ImageIO.write(image, "png", file);
     }
 
 }
